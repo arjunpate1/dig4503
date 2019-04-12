e7f730e4f3bd8e735824e84f627fe592d308b8a6
Arjun Patel <arjunsamirpatel100@gmail.com>
2019-04-12 18:03:01 -0400

    Created Base Code
    Created base html with sections and gave class and id names.

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+
+        <!-- TITLE & AUTHOR -->
+        <title>Dash - Dashboard</title>
+        
+        <!-- META DATA -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- FAVICON -->
+        <link rel="apple-touch-icon" sizes="57x57" href="client/img/favicon/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="client/img/favicon/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="client/img/favicon/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="client/img/favicon/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="client/img/favicon/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="client/img/favicon/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="client/img/favicon/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="client/img/favicon/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="client/img/favicon/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="client/img/favicon/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="client/img/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="client/img/favicon/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="client/img/favicon/favicon-16x16.png">
+        <link rel="manifest" href="client/img/favicon/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="client/img/favicon/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+        
+        <!-- Preload Stylesheet -->
+        <link rel="stylesheet" href="client/css/preload.css">
+        
+        <!-- Main Stylesheet -->
+        <link href="client/css/main.css" rel="stylesheet" type="text/css">
+        
+        <!-- Font Awesome Icons -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        
+        <!-- Google Material Icons -->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        
+        <!-- jQuery CDN -->
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+        
+        <!-- JavaScript -->
+        <script src="client/js/main.js"></script>
+        
+    </head>
+    
+    <body>
+        
+        <!-- Login Screen -->
+        <section class="login" id="login-page">
+        </section>
+        
+        <!-- Setup Page -->
+        <section class="setup" id="setup-page">
+            
+            <!-- Setup Progress Bar -->
+            <div class="setup__progress-bar">
+                
+                <div class="setup__progress-bar--inner"></div>
+                
+            </div>
+            
+            <!-- Setup Slide 1 -->
+            <div class="setup__slide setup__slide--1"></div>
+            
+            <!-- Setup Slide 2 -->
+            <div class="setup__slide setup__slide--2"></div>
+            
+            <!-- Setup Slide 3 -->
+            <div class="setup__slide setup__slide--3"></div>
+            
+            <!-- Setup Slide 4 -->
+            <div class="setup__slide setup__slide--4"></div>
+            
+        </section>
+        
+        <!-- Dashboard -->
+        <main class="dashboard" id="dashboard-page">
+            
+            <!-- Settings Page -->
+            <section class="settings" id="dashboard-settings">
+            </section>
+            
+            <!-- Clock and Greeting -->
+            <section class="clock" id="dashboard-clock">
+            </section>
+            
+            <!-- Weather Forecast -->
+            <section class="weather" id="dashboard-weather">
+            </section>
+            
+            <!-- News Section -->
+            <section class="news" id="dashboard-news">
+            </section>
+            
+            <!-- Quote of The Day -->
+            <section class="quote" id="dashboard-quote">
+            </section>
+            
+        </main>
+        
+    </body>
+    
+</html>
