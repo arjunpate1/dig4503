6ca5431c9dbd47f29e29ead94b201019b537894c
Arjun Patel <arjunsamirpatel100@gmail.com>
2019-04-24 07:28:54 -0400

    Styled Dashboard

--- client/js/unsplash.js
+++ client/js/unsplash.js
@@ -1,7 +1,7 @@
 var unsplashSearch = {
     APIkey: 'ebffa201a7d10119ea7b232582d312671b379691e32538daa3484790e959eda7',
-    query: '',
-    quantity: 20, // MAX of 30
+    query: 'earth',
+    quantity: 30, // MAX of 30
     page: 1,
     status: false,
     success: false
@@ -21,7 +21,7 @@ var unsplashOutput = {
 function searchUnsplash() {
     
     // AJAX Call To Unsplash API
-    $.getJSON('https://api.unsplash.com/search/photos?query=' + unsplashSearch.query + '&page=' + unsplashSearch.page + '&per_page=' + unsplashSearch.quantity + '&client_id=' + unsplashSearch.APIkey, function(data) {
+    $.getJSON('https://api.unsplash.com/search/photos?query=' + unsplashSearch.query + '&page=' + unsplashSearch.page + '&orientation=landscape&per_page=' + unsplashSearch.quantity + '&client_id=' + unsplashSearch.APIkey, function(data) {
         
         // Log Data object to console
         console.log(data);
@@ -37,7 +37,7 @@ function searchUnsplash() {
             
             unsplashSearch.success = false;
             
-            displayUnsplashResults();
+            $(".dashboard").css("background-image", "linear-gradient(rgba(0,0,0,. 2), rgba(0,0,0, .2)), url('https://images.unsplash.com/photo-1555999003-3f2bc447570e?ixlib=rb-1.2.1&auto=format&fit=crop&w=3300&q=80')");
             
         }
         
@@ -46,80 +46,22 @@ function searchUnsplash() {
         }
         
         // Loop through results
-        $.each(unsplashOutput.list, function(i, val) {
-
-            unsplashOutput.image = val;
-            unsplashOutput.url = val.urls.full;
-
-            unsplashOutput.url = unsplashOutput.url.replace(/\?.*$/g,"");
-            console.log("The url is: " + unsplashOutput.url);
-            
-            unsplashOutput.urlFinal = unsplashOutput.url + '?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=crop&w=' + unsplashOutput.width + '&h=' + unsplashOutput.height + '&ixid=eyJhcHBfaWQiOjY0Mzc4fQ';
-                
-            unsplashOutput.outputArray[i] = '<div class="result-image"><a class="result-image-open" href="' + unsplashOutput.url + '"><i class="material-icons">open_in_new</i></a><a class="result-image-save" href="' + unsplashOutput.url + '"><i class="material-icons">check</i></a><img src="' + unsplashOutput.urlFinal + '"></div>';
-                
-            $('<img>').on('load', displayUnsplashResults).attr('src', unsplashOutput.urlFinal);
-                
-            unsplashSearch.status = true;
-            
-            console.log(unsplashOutput.outputArray[i]);
         
-        });
+        var whichImage = Math.floor(Math.random() * unsplashOutput.list.length);
+        
+        console.log(unsplashOutput.list[whichImage].urls.full + '?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=crop&w=2000&h=1800&ixid=eyJhcHBfaWQiOjY0Mzc4fQ');
+        
+        unsplashOutput.url = unsplashOutput.list[whichImage].urls.full + '?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=crop&w=2000&h=1800&ixid=eyJhcHBfaWQiOjY0Mzc4fQ';
+        
+        $(".dashboard").css("background-image", "linear-gradient(rgba(0,0,0,.3), rgba(0,0,0,.3)), url('" + unsplashOutput.url + "')");
+        
         
     });
 }
 
-function displayUnsplashResults() {
+$(document).ready(function(){
     
-    unsplashOutput.totalLoaded++;
+    //searchUnsplash();
     
-    if (unsplashOutput.totalLoaded == unsplashOutput.list.length) {
-        
-        
-        // FADE OUT LOADER
-        $( "#loading" ).fadeOut(500);
-        $( "#searchfailed" ).fadeOut(500);
-        
-        // Empty Parent Div
-        setTimeout(function(){
-            $( ".grid" ).empty();
-            $( ".load-place" ).empty();
-        }, 500);
-        
-        // DISPLAY RESULTS ON PAGE
-        setTimeout(function(){
-            
-            for(var i = 0; i < unsplashOutput.outputArray.length; i++) {
-                
-                $(unsplashOutput.outputArray[i]).hide().appendTo(".grid").fadeIn(700);
-                
-            }
-            
-            // Reset Counter
-            unsplashOutput.totalLoaded = 0;
-            unsplashOutput.newSearchDelay = 1000;
-            interactWithResults();
-            
-        }, 700);
-        
-    }
-    
-    else if (!unsplashSearch.success) {
-        // FADE OUT LOADER
-        $( "#loading" ).fadeOut(500);
-        
-        // Empty Parent Div
-        setTimeout(function(){
-            
-            $( ".grid" ).empty();
-            $( ".load-place" ).empty();
-            var appendError = "<h2 id='searchfailed'>No Results Found</h2>";
-            $(appendError).hide().appendTo(".load-place").fadeIn(500);
-            
-        }, 500); 
-        
-        
-    }
-    
-}
+});
 
